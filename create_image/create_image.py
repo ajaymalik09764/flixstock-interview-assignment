e4b9363b6fb3d2037ed1ffd23ecd7ef03ed93435
ajaymalik09764 <ajay.malik09764@gmail.com>
2020-10-06 14:37:32 +0530

    modify code

--- create_image/create_image.py
+++ create_image/create_image.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # -----------------------read input image--------------
 img=cv2.imread("input.jpg")
@@ -6,14 +7,51 @@ img=cv2.imread("input.jpg")
 gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 #read mask image
-mask=cv2.imread("mask.png",0)
+mask=cv2.imread("mask.png")
+
 #resize mask
-resize_mask=cv2.resize(mask, (gray.shape[1],gray.shape[0]), interpolation = cv2.INTER_AREA)
+resize_mask=cv2.resize(mask,(gray.shape[1],gray.shape[0]), interpolation = cv2.INTER_AREA)
 
 
-img2_fg = cv2.bitwise_or(img,img,mask = resize_mask)
+# dst = cv2.addWeighted(img, 0.8,resize_mask, 0.05, 0)
+mask_gray=cv2.cvtColor(resize_mask,cv2.COLOR_BGR2GRAY)
+
+#erode
+kernel = np.ones((5,5),np.uint8)
+erode = cv2.erode(mask_gray,kernel,iterations = 2)
+
+img2_fg = cv2.bitwise_or(img,img,mask = erode)
 final_image=img2_fg.copy()
+
 ret, thresh = cv2.threshold(cv2.cvtColor(final_image, cv2.COLOR_BGR2GRAY), 0, 10, cv2.THRESH_BINARY)
 final_image[thresh == 0] = (0,0,255)
 
-cv2.imwrite("output.jpg",final_image)
+#blank red background image
+full_layer = np.full((5170, 4480,3), (0,0,255), np.uint8)
+cnts = cv2.findContours(cv2.cvtColor(resize_mask, cv2.COLOR_BGR2GRAY), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+
+# Find bounding box and extract ROI
+for c in cnts:
+    x,y,w,h = cv2.boundingRect(c)
+    ROI = final_image[y:y+h, x:x+w]
+    break
+
+h, w,ly = ROI.shape
+# print(h,w,ly)
+
+hh, ww,ly = full_layer.shape
+# print(hh,ww,ly)
+
+yoff = round((hh-h)/2)
+xoff = round((ww-w)/2)
+# print(yoff,xoff)
+
+#overlay image at center
+result = full_layer.copy()
+result[yoff:yoff+h, xoff:xoff+w] = ROI
+
+#output
+cv2.imwrite("output.jpg",result)
--- multi-threading-python/multithreadtask.py
+++ multi-threading-python/multithreadtask.py
@@ -9,22 +9,25 @@ class myThread(threading.Thread):
       self.exitFlag=False
 
 
-   def run(self):
+   def run(self,stop_point=None):
 
+        counter=0
+        while True:
+            if counter%5==0:
+                print("%s is running at %s --- %s" % (self.name,counter,time.time()))
 
-      print("Starting " + self.name)
+            if stop_point!= None:
+                if counter>=stop_point:
 
-      counter=0
-      delay=5
-      while counter<50:
-          if self.exitFlag:
-              break
+                    break
+            if self.exitFlag or counter>50:
 
-          print("%s is running at %s" % (self.name,counter))
-          time.sleep(delay)
-          counter+=delay
+                break
 
-      print("Exiting " + self.name)
+            time.sleep(1)
+            counter+=1
+        print("%s is Exiting at %s " % (self.name,time.time()))
+
 
    def stop(self):
      self.exitFlag=True
@@ -32,6 +35,7 @@ class myThread(threading.Thread):
 
 
 if __name__ == "__main__":
+
     thread1=myThread(1,"Thread 1")
     thread2=myThread(2,"Thread 2")
     thread3=myThread(3,"Thread 3")
@@ -44,10 +48,51 @@ if __name__ == "__main__":
     #After 20 second stop thread 1 start thread 2
     time.sleep(20)
     thread1.stop()
+    thread1.join()
     thread2.start()
 
     #Again after 18 second stop thread 3 and start thread 1
     time.sleep(18)
     thread3.stop()
+    thread3.join()
     thread1=myThread(1,"Thread 1")
     thread1.start()
+
+
+
+
+#output-----
+#i have added sec into output so that we can seen 5 sec distance.
+"""
+
+
+Thread 1 is running at 0 --- 1601974507.2539134
+Thread 3 is running at 0 --- 1601974507.2649164
+Thread 1 is running at 5 --- 1601974512.268181
+Thread 3 is running at 5 --- 1601974512.2701814
+Thread 1 is running at 10 --- 1601974517.2705617
+Thread 3 is running at 10 --- 1601974517.2725623
+Thread 1 is running at 15 --- 1601974522.2728307
+Thread 3 is running at 15 --- 1601974522.2748334
+Thread 1 is running at 20 --- 1601974527.2759948
+Thread 1 is Exiting at 1601974527.2779932
+Thread 3 is running at 20 --- 1601974527.2779932
+Thread 2 is running at 0 --- 1601974527.2789917
+Thread 3 is running at 25 --- 1601974532.2803695
+Thread 2 is running at 5 --- 1601974532.2823703
+Thread 3 is running at 30 --- 1601974537.2826405
+Thread 2 is running at 10 --- 1601974537.2846391
+Thread 3 is running at 35 --- 1601974542.2849076
+Thread 2 is running at 15 --- 1601974542.2869077
+Thread 3 is Exiting at 1601974545.2866678
+Thread 1 is running at 0 --- 1601974545.2875612
+Thread 2 is running at 20 --- 1601974547.2901773
+Thread 1 is running at 5 --- 1601974550.2899384
+Thread 2 is running at 25 --- 1601974552.2924466
+Thread 1 is running at 10 --- 1601974555.2922077
+Thread 2 is running at 30 --- 1601974557.2947085
+Thread 1 is running at 15 --- 1601974560.296477
+
+
+
+"""
